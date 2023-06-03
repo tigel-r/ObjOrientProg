c1227a8e4714eaaf729c4803e66fd823d4cd4b6d
tigel-r <tigel-r@mail.ru>
2023-06-03 23:52:27 +0300

    added class "product"

---
+++ Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Product
+    {
+        public string Type;
+        public float Price;
+        public int Mass;
+        public bool Fragile;
+
+        public bool DeliveryPoss(int SignifMass, bool SignifFragility, int WeightLimit)
+        {
+            bool DelivPoss;
+
+            if (SignifMass > WeightLimit && SignifFragility)
+                DelivPoss = false;
+
+            else
+                DelivPoss = true;
+
+            return DelivPoss;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Test
+{
+
+    abstract class Delivery
+    {
+        public string Address;
+    }
+
+    class HomeDelivery : Delivery
+    {
+      
+    }
+
+    class PickPointDelivery : Delivery
+    {
+        
+    }
+
+    class ShopDelivery : Delivery
+    {
+        
+    }
+
+    class Order <TDelivery, TStruct> where TDelivery : Delivery
+    {
+        public TDelivery Delivery;
+
+        public int Number;
+
+        public string Description;
+
+        public void DisplayAddress()
+        {
+            Console.WriteLine(Delivery.Address);
+        }
+
+    }
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a = 8;
+
+            
+
+            Product productp = new Product();
+
+            
+        ;
+            int test1 = 4;
+            bool test2 = true;
+            productp.DeliveryPoss(test1,test2,a);
+
+
+            
+
+
+
+
+        }
+
+    }
+}
